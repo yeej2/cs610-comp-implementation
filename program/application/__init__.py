e092300c49812f6d328f1f5495f4584e64f22705
Joshua Yee <yeej2@allegheny.edu>
2019-04-05 11:29:27 -0400

    starting from scratch, refactor code in test environment

--- program/application/__init__.py
+++
@@ -1,8 +0,0 @@
-from flask import Flask, url_for, request, render_template
-app = Flask(__name__)
-
-from application import db_connect
-import application.index
-import application.students
-import application.teachers
-import application.login
